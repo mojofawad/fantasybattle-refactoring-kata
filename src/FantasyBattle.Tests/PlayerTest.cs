0e9a8e5d948722b67d92cbe57a405a1d2deaebb4
joey fawad <mojofawad@gmail.com>
2025-05-05 21:11:56 -0400

    test: add test case for friendly fire to cover the rest of the Player class

--- src/FantasyBattle.Tests/PlayerTest.cs
+++ src/FantasyBattle.Tests/PlayerTest.cs
@@ -26,19 +26,37 @@ namespace FantasyBattle.Tests
     public class PlayerTest
     {
         [Fact]
-        public void DamageCalculations() 
+        public void DamageCalculations_TargetIsBasicEnemy_CalculatesDamage() 
         {
             // Arrange
-            Equipment equipment = new Equipment(TestData.sword, TestData.excalibur, TestData.helmet, TestData.boots, TestData.breastplate);
-            Inventory inventory = new Inventory(equipment);
-            Stats stats = new Stats(1);
+            var equipment = new Equipment(TestData.sword, TestData.excalibur, TestData.helmet, TestData.boots, TestData.breastplate);
+            var inventory = new Inventory(equipment);
+            var stats = new Stats(1);
             var player = new Player(inventory, stats);
+            var targetEnemy = TestData.enemy;
             
             // Act
-            Damage damage = player.CalculateDamage(TestData.enemy);
+            var damage = player.CalculateDamage(targetEnemy);
             
             // Assert
             Assert.Equal(149, damage.Amount);
+        }
+
+        [Fact]
+        public void DamageCalculations_TargetIsPlayer_ReturnsZeroDamage()
+        {
+            // Arrange
+            var equipment = new Equipment(TestData.sword, TestData.excalibur, TestData.helmet, TestData.boots, TestData.breastplate);
+            var inventory = new Inventory(equipment);
+            var stats = new Stats(1);
+            var player = new Player(inventory, stats);
+            var targetPlayer = new Player(inventory, stats);
+            
+            // Act
+            var damage = player.CalculateDamage(targetPlayer);
+            
+            // Assert
+            Assert.Equal(0, damage.Amount);
         }
     }
 }
